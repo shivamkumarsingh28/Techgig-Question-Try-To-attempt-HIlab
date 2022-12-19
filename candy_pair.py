2cf28199b536d7e4a2b6155356e2a668d795d318
shivamkumarsingh28 <110661272+shivamkumarsingh28@users.noreply.github.com>
2022-12-19 12:08:31 +0530

    Create candy_pair.py

---
+++ candy_pair.py
@@ -0,0 +1,35 @@
+
+
+# Function to find the Largest Alphabetic Character
+def largestCharacter():
+	
+	# Array for keeping track of both uppercase
+	# and lowercase english alphabets
+	uppercase = [False] * 26
+	lowercase = [False] * 26
+	
+	arr = list(input())
+	for c in arr:
+		if (c.islower()):
+			lowercase[ord(c) - ord('a')] = True
+		if (c.isupper()):
+			uppercase[ord(c) - ord('A')] = True
+			
+	# Iterate from right side of array
+	# to get the largest index character
+	for i in range(25,-1,-1):
+		
+		# Check for the character if both its
+		# uppercase and lowercase exist or not
+		if (uppercase[i] and lowercase[i]):
+			return chr(i + ord('A')) + ""
+	# Return -1 if no such character whose
+	# uppercase and lowercase present in
+	# string str
+	return "0"
+
+# Driver code
+
+
+print(largestCharacter())
+	
